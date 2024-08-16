21614c2d541e5f899260c147ab0e7da2e319509b
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-08-16 14:27:07 +0900

    store account data

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,11 @@ export const COMPILER = 'aptos';
 export const COMPILER_URL = 'https://aptos.dev/en/build/cli';
 export const MoveToml = 'Move.toml';
 export const ByteDump = 'bytecode.dump.json';
+
+export const runCompile = (path: string) => {
+  return `${COMPILER} move build-publish-payload --json-output-file ${path}/${ByteDump}`;
+};
+
+export const runTest = (path: string) => {
+  return `${COMPILER} move test --package-dir ${path}`;
+};
