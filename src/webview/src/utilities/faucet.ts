5b695fd67f7f7f88133fbd0f77c8946a95ecadff
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-09-02 23:03:11 +0900

    Update faucet.ts

--- src/webview/src/utilities/faucet.ts
+++ src/webview/src/utilities/faucet.ts
@@ -3,7 +3,7 @@ import { IAccount } from '../recoil';
 export const faucet = async (account: IAccount): Promise<boolean> => {
   if (account.nonce.network !== 'mainnet' && account.zkAddress) {
     try {
-      const amount = 10000;
+      const amount = 100000000;
       const res = await fetch(
         `https://faucet.${account.nonce.network}.aptoslabs.com/mint?amount=${amount}&address=${account.zkAddress.address}`,
         {
