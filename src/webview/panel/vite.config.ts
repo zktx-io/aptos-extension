a0f4484563bb5fe23be4136d69f308edefdfd497
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-12-29 21:52:08 +0900

    update github action

--- src/webview/panel/vite.config.ts
+++ src/webview/panel/vite.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'main.js',
-        chunkFileNames: 'chunks/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+export default defineConfig(() => {
+  const enableSourceMap = process.env.SOURCE_MAP !== 'false';
+  return {
+    plugins: [react()],
+    build: {
+      minify: true,
+      sourcemap: enableSourceMap,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'main.js',
+          chunkFileNames: 'chunks/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+        },
       },
     },
-  },
+  };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
+  const enableSourceMap = process.env.SOURCE_MAP !== 'false';
   const isNode = mode === 'node';
   const outDir = isNode ? './out/node' : './out/web';
   return {
     build: {
+      minify: 'esbuild',
+      sourcemap: enableSourceMap,
       outDir,
       lib: {
         entry: './src/extension.ts',
